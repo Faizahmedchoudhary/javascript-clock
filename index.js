2a99825bf5e238ccb9029d850f2602ed39995787
Faiz <ahmedfaiz868@gmail.com>
2022-07-26 17:25:17 +0530

    initial

---
+++ index.js
@@ -0,0 +1,28 @@
+    showtime = () => {
+
+        
+        var today = new Date();
+        
+        
+        var a = today.getHours();
+        var b =  today.getMinutes();
+        var c =  today.getSeconds();
+
+     
+    document.querySelector('.hour').innerHTML = a ; 
+    document.querySelector('.minutes').innerHTML = b;
+    document.querySelector('.seconds').innerHTML = c;
+
+
+
+
+    if(a > 12){
+        document.querySelector('.day').innerHTML = 'PM';
+    }
+    else{
+        document.querySelector('.day').innerHTML = 'AM';
+    }
+    setTimeout( showtime , 1000);
+}
+
+showtime();
